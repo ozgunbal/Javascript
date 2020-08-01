83344cf246b4f1c4a45653be7ef8930c18cc246f
Ozgun Bal <ozgunbal@gmail.com>
2020-08-02 01:00:50 +0300

    feat: Add infographic comparison

--- app.js
+++ app.js
@@ -1,32 +1,212 @@
+/**
+ * @description Represents a Dino
+ * @constructor
+ * @param {string} species - dino species
+ * @param {number} weight - weight in lbs
+ * @param {number} height - height in inches
+ * @param {string} dier - diet of dino
+ * @param {string} where - place of where dino lived
+ * @param {string} when - era of when dino lived
+ * @param {string} fact - dino fact
+ */
+function Dino(species, weight, height, diet, where, when, fact) {
+  this.species = species;
+  this.weight = weight;
+  this.height = height;
+  this.diet = diet;
+  this.where = where;
+  this.when = when;
+  this.fact = fact;
+}
 
-    // Create Dino Constructor
+/**
+* @description Decides dino is heavier than human (Dino Compare Method 1)
+* @param {object} human Human Object
+* @returns {string} Comparison result
+*/
+Dino.prototype.hasHeaiverThan = function (human) {
+  return this.weight > human.weight
+    ? "Heavier than human"
+    : "Lighter than human";
+};
 
+/**
+* @description Decides dino is taller than human (Dino Compare Method 2)
+* @param {object} human Human Object
+* @returns {string} Comparison result
+*/
+Dino.prototype.hasTallerThan = function (human) {
+  return this.height > human.height
+    ? "Taller than human"
+    : "Shorter than human";
+};
 
-    // Create Dino Objects
+/**
+* @description Decides dino has same diet with human (Dino Compare Method 3)
+* @param {object} human Human Object
+* @returns {string} Comparison result
+*/
+Dino.prototype.hasSameDiet = function (human) {
+  return this.diet === human.diet
+    ? "Same Diet with human"
+    : "Different Diet from human";
+};
 
+/**
+ * @description Represents a Human
+ * @constructor
+ * @param {string} name - name of the human
+ * @param {number} feet - feet height of the human
+ * @param {number} inches - inches height of the human
+ * @param {number} weight - weight of the human
+ * @param {string} diet - herbavor, omnivor or carnivor
+ */
+function Human(name, feet, inches, weight, diet) {
+  this.name = name;
+  (this.species = "Human"), (this.height = feet * 12 + inches);
+  this.weight = weight;
+  this.diet = diet;
+}
 
-    // Create Human Object
+// HTML Nodes
+const form = document.querySelector("#dino-compare");
+const grid = document.querySelector("#grid");
+const formFieldIds = ["#name", "#feet", "#inches", "#weight", "#diet"];
 
-    // Use IIFE to get human data from form
+const TILE_TYPES = {
+  DINO: "Dino",
+  HUMAN: "Human",
+  PIGEON: "Pigeon",
+};
 
+/**
+ * @description Gets Dino data asynchronously
+ * @returns {Promise<Dino[]>} Promise of Array of Dino Objects
+ */
+const getDinoData = async () => {
+  const jsonResponse = await fetch("dino.json");
+  const { Dinos } = await jsonResponse.json();
+  return Dinos.map(
+    ({ species, weight, height, diet, where, when, fact }) =>
+      new Dino(species, weight, height, diet, where, when, fact)
+  );
+};
 
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
+/**
+ * @description Generates grid item including title, image and fact
+ * @param {object} obj Dino(, Pigeon) or Human object
+ * @param {number} human Human object for comparison when obj is Dino
+ * @returns {HTMLDivElement} Grid item of either dino, pigon or human
+ */
+const generateTile = (obj, human) => {
+  const gridItem = document.createElement("div");
+  gridItem.classList.add("grid-item");
+  const head = document.createElement("h3");
+  const image = document.createElement("img");
+  const fact = document.createElement("p");
 
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+  if (obj.species === TILE_TYPES.HUMAN) {
+    head.innerText = obj.name;
 
-    
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+    image.src = `/images/human.png`;
+    image.alt = "human";
+  } else {
+    head.innerText = obj.species;
 
+    image.src = `/images/${obj.species.toLowerCase()}.png`;
+    image.alt = obj.species;
 
-    // Generate Tiles for each Dino in Array
-  
-        // Add tiles to DOM
+    fact.innerText =
+      obj.species === TILE_TYPES.PIGEON ? obj.fact : getRandomFact(obj, human);
+  }
 
-    // Remove form from screen
+  gridItem.appendChild(head);
+  gridItem.appendChild(image);
+  fact.innerText && gridItem.appendChild(fact);
 
+  return gridItem;
+};
+
+/**
+* @description Shuffles order of the items in the array
+* @param {array} array
+* @returns {array} Shuffed array
+*/
+const shuffleArray = (array) => {
+  const newArray = [...array];
+  for (let i = newArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+  }
+  return newArray;
+}
+
+/**
+* @description Gets random fact about dino
+* @param {object} dino
+* @param {object} human
+* @returns {string} fact descriptino
+*/
+const getRandomFact = (dino, human) => {
+  const luckyNumber = Math.floor(Math.random() * 6);
+  switch (luckyNumber) {
+    case 0:
+      return dino.hasHeaiverThan(human);
+    case 1:
+      return dino.hasTallerThan(human);
+    case 2:
+      return dino.hasSameDiet(human);
+    case 3:
+      return `Live at ${dino.when}`;
+    case 4:
+      return `Born in ${dino.where}`;
+    case 5:
+    default:
+      return dino.fact;
+  }
+};
+
+/**
+* @description Generates all tiles add to DOM
+* @param {object[]} dinos Array of Dino Object
+* @param {object} human
+*/
+const generateTiles = (dinos, human) => {
+  // Generate Tiles for each Dino in Array
+  const dinoTiles = shuffleArray(dinos).map((dino) =>
+    generateTile(dino, human)
+  );
+  const humanTile = generateTile(human);
+  const tiles = [...dinoTiles.slice(0, 4), humanTile, ...dinoTiles.slice(4)];
+
+  // Add tiles to DOM
+  tiles.forEach((tile) => grid.appendChild(tile));
+};
+
+/**
+ * @description Execution after form submition
+ */
+const main = async () => {
+  // Create Dino Objects
+  const dinos = await getDinoData();
+
+  // Create Human Object
+  // Use IIFE to get human data from form
+  const human = (function getFields() {
+    const [name, feet, inches, weight, diet] = formFieldIds
+      .map((id) => document.querySelector(id))
+      .map((field) => field.value);
+      console.log(name, +feet, +inches, +weight, diet);
+    return new Human(name, +feet, +inches, +weight, diet);
+  })();
+
+  generateTiles(dinos, human);
+  // Remove form from screen
+  form.classList.add("hidden");
+};
 
 // On button click, prepare and display infographic
+form.addEventListener("submit", function formHandler(evt) {
+  evt.preventDefault();
+  main();
+});
